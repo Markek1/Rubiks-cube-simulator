756232713b2ceb81aefe370ced0d843ddbbb73b8
Markek1 <mareklicko448@gmail.com>
2020-08-06 10:15:29 +0200

    added button drawing

--- gui.py
+++ gui.py
@@ -27,34 +27,53 @@ def draw_cube(cube_to_draw, start_x, start_y, size_x, size_y):
     draw_side(cube_to_draw.sides['D'], start_x + x, start_y + 2 * y)
 
 
-def draw_buttons(button, color):
-    if not edge_color:
-        edge_color = color
-    pygame.draw.rect(screen, color, button)
-
-
-def draw_text(text, font, color, surface, x, y):
-    textobj = font.render(text, 1, color)
-    textrect = textobj.get_rect()
-    textrect.topleft = (x, y)
-    surface.blit(textobj, textrect)
 
 
 FPS = 5
 BACKGROUND_COLOR = (64, 64, 64)
-BUTTON_COLOR = (0, 200, 255)
 WIDTH, HEIGHT = 500, 500
 main_clock = pygame.time.Clock()
 pygame.init()
 pygame.display.set_caption('Rubiks cube simulator')
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-font = pygame.font.SysFont(None, 20)
+
+BUTTON_COLOR = (0, 240, 255)
+BUTTON_WIDTH, BUTTON_HEIGHT = WIDTH // 8, HEIGHT // 15
+BUTTONS = {}
+BUTTON_NAMES = [['new', 'clear'],
+                ['do', 'scramble'],
+                ['solve', 'options']]
+BUTTONS_SHAPE = (len(BUTTON_NAMES), len(BUTTON_NAMES[0]))
+
+
+FONT = pygame.font.SysFont('arialrounded', 12)
+TEXTS = {}
+TEXT_COLOR = (0, 0, 0)
+
+
+def draw_text(text, font, color, surface, coordinates):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = coordinates
+    surface.blit(textobj, textrect)
+
+
+def create_buttons(names, start_x, start_y):
+    max_j, max_i = len(names), len(names[0])
+    j, i = BUTTONS_SHAPE
+    for j in range(max_j):
+        for i in range(max_i):
+            BUTTONS[names[j][i]] = pygame.Rect(start_x + i * BUTTON_WIDTH, start_y + j * BUTTON_HEIGHT,
+                                        BUTTON_WIDTH - 2, BUTTON_HEIGHT - 2)
+            TEXTS[names[j][i]] = (start_x + i * BUTTON_WIDTH, start_y + j * BUTTON_HEIGHT)
+
+
+create_buttons(BUTTON_NAMES,
+               WIDTH - BUTTONS_SHAPE[1] * BUTTON_WIDTH // 0.9,
+               HEIGHT - BUTTONS_SHAPE[0] * BUTTON_HEIGHT // 0.9)
 
 c = cube.Cube()
 moves_to_do = "R2 U R U R' U' R' U' R' U R'"
-
-buttons = {}
-
 
 def main_menu():
     while True:
@@ -79,22 +98,19 @@ def main_menu():
             c.rotate_side(side_name, rotation)
             draw_cube(c, 50, 50, 360, 270)
 
-            draw_text('Main Menu', font, (255, 255, 255), screen, 20, 20)
-
-            # mx, my = pygame.mouse.get_pos()
-
-            button_new = pygame.Rect(WIDTH - WIDTH // 4, HEIGHT - HEIGHT // 4, WIDTH // 10, HEIGHT // 20)
-            # button_2 = pygame.Rect(50, 200, 200, 50)
+            mx, my = pygame.mouse.get_pos()
 
             if click:
-                if button_1.collidepoint((mx, my)):
-                    pass
+                if BUTTONS['new'].collidepoint((mx, my)):
+                    c.generate_solved_cube()
 
-            pygame.draw.rect(screen, BUTTON_COLOR, button_new)
-            # pygame.draw.rect(screen, (255, 0, 0), button_2)
+            for text, button in zip(TEXTS.keys(), BUTTONS.values()):
+                pygame.draw.rect(screen, BUTTON_COLOR, button)
+                draw_text(text, FONT, TEXT_COLOR, screen, TEXTS[text])
 
             pygame.display.update()
             main_clock.tick(FPS)
 
 
+# def do_moves()
 main_menu()
